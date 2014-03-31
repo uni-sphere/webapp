bba006dafd1ccbf0d7de4c80921a2a7e476688a0
Clempops <clement_muller@hotmail.fr>
2014-03-31 18:23:41 +0200

    debug usercontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_action :set_user, only: [:show, :destroy]
+  before_action :set_user, only: [:show, :destroy, :update, :edit]
   before_action :user_params, only: [:create, :update]
   before_filter :correct_user, :only => [:edit, :update]
   before_filter :authenticate?, :only => [:edit, :update, :index, :destroy]
