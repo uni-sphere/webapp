fd2e73ff45ae2c22022f8e06749b6cc18e680184
Gabriel <gabriel.muller.12@gmail.com>
2014-10-15 15:45:14 +0200

    temporary design fix focus = hover

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,10 +1,35 @@
 $(document).ready(function(){
   $('[data-toggle=offcanvas]').click(function() {
-    $(this).toggleClass('text-center ');
-    $(this).find('i').toggleClass('fa-chevron-right fa-chevron-left');
+    // $(this).toggleClass('text-center ');
+    $('#chevron').find('i').toggleClass('fa-chevron-left').toggleClass('fa-chevron-right');
     $('.row-offcanvas').toggleClass('active');
-    $('#sidebar-main-wrapper').toggleClass('hidden-small').toggleClass('visible-small');
+    $('#tabs-small').toggleClass('hidden-small').toggleClass('visible-small');
+    $('#tabs-small-toggle').toggleClass('hidden-small').toggleClass('visible-small');
+    $('#group-choice-small').toggleClass('hidden-small').toggleClass('visible-small');
+    $('#tabs-perso-groups').toggleClass('hidden-small').toggleClass('visible-small');
     $('#sidebar-small').toggleClass('visible-small').toggleClass('hidden-small');
     $('#navbar').toggleClass('top-fixed').toggleClass('top-relative');
+    
   });
+
+  $(window).resize(function() {
+    if($(window).width() >= 768) {
+      $('.row-offcanvas').removeClass('active');
+      $('#chevron').find('i').removeClass('fa-chevron-left').addClass('fa-chevron-right');
+      $('#navbar').addClass('top-fixed').removeClass('top-relative');
+      $('#tabs-small').addClass('hidden-small').removeClass('visible-small');
+      $('#tabs-perso-groups').addClass('hidden-small').removeClass('visible-small');
+      $('#sidebar-small').addClass('visible-small').removeClass('hidden-small');
+      $('#tabs-small').addClass('visible-xs').addClass('hidden-small').removeClass('visible-small');
+      $('#tabs-small-toggle').addClass('visible-xs').addClass('visible-small').removeClass('hidden-small');
+      $('#group-choice-small').removeClass('hidden-small').addClass('visible-small');
+    } else{
+      $('#tabs-small').removeClass('visible-xs');
+      $('#tabs-small-toggle').removeClass('visible-xs');
+    } 
+
+  }).resize();
+
+
+
 });
