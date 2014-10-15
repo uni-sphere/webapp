638b73b5a37a33475d6b299ba97e76aebd8588e6
Gabriel <gabriel.muller.12@gmail.com>
2014-10-15 20:17:08 +0200

    minor change in vegeta_attack.sh

--- vegeta_attack.sh
+++ vegeta_attack.sh
@@ -1,1 +1,2 @@
-echo "GET https://infinitree.appsdeck.eu" | vegeta attack -rate 20 -duration=60s | tee results.bin | vegeta report
+echo "GET https://infinitree.appsdeck.eu" | vegeta attack -rate 20 -duration=60s | tee results.bin | vegeta report;
+rm results.bin
