96f8f5df500a79915d432af3afa3e723876efdcc
ClemPops <clement_muller@hotmail.fr>
2015-03-01 16:50:06 +0100

    email redirection to confirmation path

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ActionMailer::Base
   def welcome_email(id, name, email)
     @id = id
     @name = name
-    @url  = 'https://unisphereapp.scalingo.io/signin?confirmation=' + @id.to_s
+    @url  = 'https://unisphereapp.scalingo.io/confirmation?confid=' + @id.to_s
     mail(to: email, subject: 'Welcome to Unisphere')
   end
   
